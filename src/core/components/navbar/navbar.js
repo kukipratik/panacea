1cebf87e56c65408a6e883c5a42700be9b805110
Pratik Lama <kukipratik123@gmail.com>
2023-01-27 11:36:22 +0545

    make navbar

---
+++ src/core/components/navbar/navbar.js
@@ -0,0 +1,38 @@
+import { useMediaQuery, useTheme } from "@mui/material";
+
+import Logo from "../Logo"
+import './navbar.css';
+import MyDrawer from "./drawer";
+
+export default function Navbar() {
+    const theme = useTheme();
+    const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+
+    return (
+        <>{isMatch ? (
+            <div className="container">
+                < div className="navbar" >
+                    <Logo />
+                    <MyDrawer />
+                </div>
+            </div>
+        ) : (
+            <div className="container">
+                < div className="navbar" >
+                    <Logo width={85} />
+                    <nav>
+                        <ul>
+                            <li><a href="/marketPlace">Market Place </a></li>
+                            <li><a href="">Solutions</a></li>
+                            <li><a href="">Blogs</a></li>
+                            {/* <li><a href="">Items</a></li> */}
+                            <li><a href="/profile">Profile</a></li>
+                        </ul>
+                    </nav>
+                </div >
+            </div>
+        )}
+        </>
+
+    );
+};
