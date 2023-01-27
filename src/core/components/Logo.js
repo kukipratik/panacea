811cf6627f6ee34ba3f6b2f731d7fbd1cef3b209
Pratik Lama <kukipratik123@gmail.com>
2023-01-27 11:26:30 +0545

    make logo

---
+++ src/core/components/Logo.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import { ReactComponent as LogoSvg } from "../assets/svg/logo.svg"
+
+export default function Logo() {
+
+    return (
+        <a href="/" >
+            <Box marginY={1} marginX={1} >
+                <LogoSvg height="62" width="150" />
+            </Box >
+        </a>
+    );
+}
